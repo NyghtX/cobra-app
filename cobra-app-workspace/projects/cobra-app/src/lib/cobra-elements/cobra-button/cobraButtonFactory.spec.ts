750f76ea05a5cfad0b7f4b515bb36c7104b3aa12
NyghtX <jan.heinsohn.dev@gmail.com>
2021-12-03 20:35:16 +0100

    CobraButtonFactory Tests

--- cobra-app-workspace/projects/cobra-app/src/lib/cobra-elements/cobra-button/cobraButtonFactory.spec.ts
+++ cobra-app-workspace/projects/cobra-app/src/lib/cobra-elements/cobra-button/cobraButtonFactory.spec.ts
@@ -1,5 +1,6 @@
 import {CobraButtonFactory} from "./cobraButtonFactory";
 import {CobraButton} from "./cobraButton";
+import {CobraButtonDefinition} from "./cobraButtonDefinition";
 
 describe("CobraButtonFactory", () => {
 
@@ -8,6 +9,19 @@ describe("CobraButtonFactory", () => {
     expect(sut).toBeTruthy();
     expect(sut).toBeInstanceOf(CobraButtonFactory);
     expect(sut.config.label).toEqual("Test");
-  })
+  });
+
+  it("should get the callback right", () => {
+    let x: any = {};
+    const sut = CobraButton("Test")
+      .thatCalls(ctx => {
+        ctx.x = "Test"
+      }).build(x);
+    expect(sut).toBeTruthy();
+    expect(sut).toBeInstanceOf(CobraButtonDefinition);
+
+    sut.config.cb!(x);
+    expect(x.x).toEqual("Test")
+  });
 
 });
